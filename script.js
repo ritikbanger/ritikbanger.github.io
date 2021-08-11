55e5f934bd01da3b0312195cecdefe00ffdd3d7a
ritikbanger <47841501+ritikbanger@users.noreply.github.com>
2021-08-11 23:18:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,15 @@ $(document).ready(function(){
             }
         }
     });
- magicMouse();
+    options = {
+  "cursorOuter": "circle-basic",
+  "hoverEffect": "circle-move",
+  "hoverItemMove": false,
+  "defaultCursor": false,
+  "outerWidth": 30,
+  "outerHeight": 30
+}
+ magicMouse(options);
     
 
 });
